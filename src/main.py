d05b3f150c5909737785f7df29e1eefb4b7c96f7
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-11-15 13:04:50 +0400

    added debug led

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@
 All pin numbers are based on on Raspberry Pi 4B+ GPIO Layput. You can change these to your liking, but make sure to wire it up accordingly.
 """
 
+import time
 import RPi.GPIO as GPIO
 from src.carcontroller import CarController
+
+# This LED will be used to signal whether there is an error in the program during execution.
+DEBUG_LED = 7
 
 # These pins are to control the L298N module.
 LA = 23
@@ -19,7 +23,16 @@ def main():
     # Setup GPIO
     GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
+    GPIO.setup(DEBUG_LED, GPIO.OUT)
     car_controller.setup()
+
+    # Start up sequence
+    print("Starting...")
+    for i in range(3):
+        GPIO.output(DEBUG_LED, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(DEBUG_LED, GPIO.LOW)
+        time.sleep(0.5)
 
     # Cleanup GPIO
     GPIO.cleanup()
